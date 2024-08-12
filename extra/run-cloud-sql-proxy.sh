447f35ed7a105c411353ae6a4ae617b90fdef269
Chad Jablonski <chad@jablonski.xyz>
2024-08-12 14:45:41 -0400

    Wait 30s after SIGTERM

--- extra/run-cloud-sql-proxy.sh
+++ extra/run-cloud-sql-proxy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 printf "%s" "$GCLOUD_CREDENTIALS" | base64 --decode > /app/google/credentials.json
 echo "🔐 Starting Cloud SQL Proxy: $GCLOUD_INSTANCE"
-exec /app/google/bin/cloud-sql-proxy "$GCLOUD_INSTANCE" --credentials-file=/app/google/credentials.json &
+exec /app/google/bin/cloud-sql-proxy \
+  "$GCLOUD_INSTANCE" \
+  --credentials-file=/app/google/credentials.json \
+  --max-sigterm-delay=30 \
+  &
